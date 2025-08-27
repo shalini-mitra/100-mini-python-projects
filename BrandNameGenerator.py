7646d2a6d2affc8b23fc7e5ee7043b11a32a5237
shalini-kancharla <shalinimitra1992@gmail.com>
2025-08-27 14:08:58 -0700

    Create your own brand name

---
+++ BrandNameGenerator.py
@@ -0,0 +1,16 @@
+#task - 1
+import random
+#User input
+print("Welcome to the Band Name Generator. 🐾✨") # It will print the welcome message.
+# input is stored in the username variable
+city = input("what's the name of the city you grew up in?\n")
+petname = input("what's your pet name?\n")
+adjectives = ["Fluffy", "Sparkly", "Tiny", "Happy", "Bubbly", "Sweet"]
+emojis = ["🐶", "🐱", "🌸", "🍭", "🦄", "💖"]
+#print('Your band name could be: ' + city + ' ' + petname)
+#random selections
+adj = random.choice(adjectives)
+emoji = random.choice(emojis)
+num = random.randint(1, 99)
+
+print(f'Your band name could be: {adj} {city} {petname}{num}{emoji}')
